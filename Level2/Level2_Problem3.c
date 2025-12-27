b8e42fea17dc255e9ddfb7fd3a2cfc9c229cb9db
Dinesh Kumaran K <dineshkumarank2378@gmail.com>
2025-12-27 22:03:52 +0530

    Update Level2_Problem3.c

--- Level2/Level2_Problem3.c
+++ Level2/Level2_Problem3.c
@@ -5,13 +5,6 @@ int main(void)
     int x;
     printf("Enter the number : ");
     scanf("%d",&x);
-    if(x<=50)
-    {
-        printf("1");
-    }
-    else
-    {
-        printf("0");
-    }return 0;
-    //printf("Result=%d",y);
+    printf("%d",x<=50);
 }
+
