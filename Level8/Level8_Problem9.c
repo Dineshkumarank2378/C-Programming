4657010109b808d8a06970d06302b68718237382
Dinesh kumaran <dineshkumarank2378@gmail.com>
2026-01-05 11:04:27 +0530

    Function With Return Value

--- Level8/Level8_Problem9.c
+++ Level8/Level8_Problem9.c
@@ -10,11 +10,16 @@ int main()
 }
  int check_last_digit_odd(int x)
  {
+    if ((x%10)%2==0)
+    {
+        return x;
+    }
+    else{
     int z=0,y=1,w=x;
     for(;x>10;x/=10)
     {
         y=y*10;
     }
     z=w-y;
-    return z;
+    return z;}
  }
